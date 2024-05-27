1de4d60db916deeffb5dcf233f9d115d3d5608c0
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-05-28 00:32:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",function(event)
+    {
+        let circle = document.querySelectorAll('.data');
+        circle.forEach(function(progress){
+            let degree = 0;
+            var targetDegree = parseInt(progress.getAttribute('data-degree'));
+            let color = progress.getAttribute('data-color');
+            let number = progress.querySelector('.number');
+            var interval = setInterval(function(){
+                degree +=1;
+                if(degree > targetDegree){
+                    clearInterval(interval);
+                    return;
+                }
+                progress.style.background = `conic-gradient(${color} ${degree}%,#222 0%)`;
+                number.innerHTML = degree + '<span>%</span>';
+                number.style.color = color;
+            },40)
+        });
+    }
+);
+
